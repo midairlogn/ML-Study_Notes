db526693510e67a109e91da64e0ed5dbeed69c81
midairlogn <121656324+midairlogn@users.noreply.github.com>
2026-07-19 18:41:09 +0800

    Add template for cf cpp files

---
+++ Cf_cpp_template/README.md
@@ -0,0 +1,3 @@
+# ML-Study_Notes for Cf cpp template
+
+A template for cpp files for codeforces.
---
+++ Cf_cpp_template/ml-template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+#ifdef ML_DEBUG
+    auto _clock_start = chrono::high_resolution_clock::now();
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        
+    }
+#ifdef ML_DEBUG
+	cerr << "Executed in " << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - _clock_start).count() << " ms." << endl;
+#endif
+    return 0;
+}
